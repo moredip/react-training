c52553b3dfd1df5acc228c2d5ea2141016c47fd6
Pete Hodgson <git@thepete.net>
2017-03-05 21:12:36 -0800

    make hello more hello world-y

--- hello/src/index.js
+++ hello/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+const container = document.getElementById('root');
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <p>hello, world</p>,
+  container
 );
