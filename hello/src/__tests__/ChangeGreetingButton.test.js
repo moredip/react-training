0340c03e36205aa00a0ec053e35bf145d2712ca8
Pete Hodgson <git@thepete.net>
2017-05-21 20:31:02 -0700

    add ChangeGreetingButton test

---
+++ hello/src/__tests__/ChangeGreetingButton.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ChangeGreetingButton from '../ChangeGreetingButton';
+
+describe('ChangeGreetingButton', function () {
+  it('contains a <button>', function () {
+    const component = shallow(<ChangeGreetingButton/>);
+    expect(component.find('button')).toBePresent();
+  });
+
+  it('triggers handler when button is pressed', function () {
+    const spyHandler = jest.fn();
+    const component = shallow(<ChangeGreetingButton onChangeGreeting={spyHandler}/>);
+    component.find('button').simulate('click');
+    expect(spyHandler).toBeCalled();
+  });
+});
