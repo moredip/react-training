62c4e1f89f67867a76a21121f3f595256995c962
Pete Hodgson <git@thepete.net>
2017-05-24 10:49:44 -0700

    add more unit tests, including some "TODO" tests

---
+++ slackr/src/__tests__/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '../App';
+
+describe('App', function () {
+  it('starts off with no last message in history', function () {
+    const component = shallow(<App/>);
+    const channelHistory = component.find('ChannelHistory');
+    expect(channelHistory).toHaveProp('lastMessage',null);
+  });
+
+  it('updates history with new last message when one is composed', function () {
+    const component = shallow(<App/>);
+    const composeMessage = component.find('ComposeMessage');
+
+    composeMessage.prop('onMessage')('a new message');
+
+    const channelHistory = component.find('ChannelHistory');
+
+    expect(channelHistory).toHaveProp('lastMessage','a new message');
+  });
+
+  xit('overwrites the previous last message');
+
+});
--- slackr/src/__tests__/ComposeMessage.test.js
+++ slackr/src/__tests__/ComposeMessage.test.js
@@ -14,6 +14,8 @@ describe('ComposeMessage', function () {
     expect(textInput()).toHaveValue('');
   });
 
+  xit('has a 'write a message' placeholder');
+
   it('does not call handler when input changes but has not been submitted', function () {
     const spyHandler = jest.fn();
     component = shallow( <ComposeMessage onMessage={spyHandler} /> );
