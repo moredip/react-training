1aecc334ffed88278f96d9ffbc7d8e414c9ffd35
Pete Hodgson <git@thepete.net>
2017-06-29 11:15:15 -0700

    wire up redux dev tools

--- slackr/src/index.js
+++ slackr/src/index.js
@@ -7,7 +7,10 @@ import reducers from './reducers'
 import App from './App';
 import './index.css';
 
-let store = createStore(reducers);
+const store = createStore(
+   reducers,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
