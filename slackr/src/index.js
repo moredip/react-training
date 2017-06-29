a95f4111bb9a6594703884fcec99ace87ee5a991
Pete Hodgson <git@thepete.net>
2017-06-29 10:59:38 -0700

    WIP setting up Redux

--- slackr/src/index.js
+++ slackr/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './reducers'
 import App from './App';
 import './index.css';
 
+let store = createStore(reducers);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
