6bcb2cec644dbfe0fd0656c72b81e64997bea323
Pete Hodgson <git@thepete.net>
2017-06-29 17:04:41 -0700

    make ducks tests more integrated by using real store to test them

--- slackr/src/ducks/__tests__/channel.test.js
+++ slackr/src/ducks/__tests__/channel.test.js
@@ -1,9 +1,10 @@
+import {createStore} from 'redux';
 import channelReducer, * as channelActions from '../channel';
 
 describe('channel duck', () => {
   it('starts off with an empty list of messages', () => {
-    const initialState = channelReducer(undefined,{});
-    expect(initialState).toHaveProperty('messages',[]);
+    const store = createStore(channelReducer);
+    expect(store.getState()).toHaveProperty('messages',[]);
   });
 
   it('accumulates messages', () => {
@@ -23,6 +24,27 @@ describe('channel duck', () => {
     expect(stateAfterSecondMessage).toHaveProperty('messages',[
       'my first message',
       'another message'
+    ]);
+  });
+
+  it('accumulates messages [using store]', () => {
+    const store = createStore(channelReducer);
+
+    store.dispatch(
+      channelActions.postMessage('my first message')
+    );
+
+    expect(store.getState()).toHaveProperty('messages',[
+      'my first message'
+    ]);
+
+    store.dispatch(
+      channelActions.postMessage('the next message')
+    );
+
+    expect(store.getState()).toHaveProperty('messages',[
+      'my first message',
+      'the next message'
     ]);
   });
 });
