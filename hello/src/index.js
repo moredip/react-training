b42b82cc491fae3cc63264e2f8c76f2e12b74038
Pete Hodgson <git@thepete.net>
2017-07-08 08:47:10 -0700

    WIP redux-ifying

--- hello/src/index.js
+++ hello/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 import App from './App';
+import reducers from './reducers';
+
 
 const container = document.getElementById('root');
 
-ReactDOM.render( <App/>, container );
+let store = createStore(reducers);
+
+ReactDOM.render( 
+  <Provider store={store}>
+    <App/>
+  </Provider>, 
+  container 
+);
