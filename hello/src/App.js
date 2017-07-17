b326e8dfdee3fc01ee33071108e5314a32ebb862
Pete Hodgson <git@thepete.net>
2017-07-17 11:01:03 -0700

    add handler

--- hello/src/App.js
+++ hello/src/App.js
@@ -7,9 +7,13 @@ export default function App(props){
   return (
     <div>
       <Salutation greeting={greeting}/>
-      <button>Add Greeting</button>
+      <button onClick={handleButtonClick}>Add Greeting</button>
     </div>
   );
+}
+
+function handleButtonClick(){
+  window.alert('button was clicked!');
 }
 
 function Salutation(props){
