0cd6cccd63e39cfd28127b01065a75d0781bc5d7
Pete Hodgson <git@thepete.net>
2017-07-25 07:12:09 -0700

    install thunk

--- slackr/src/boot.js
+++ slackr/src/boot.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import reducers from './reducers'
 import App from './App';
 
 export default function createReduxedApp(){
   const store = createStore(
-     reducers,
-     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    reducers,
+    enhancers()
   );
 
   return (
     <Provider store={store}>
       <App />
     </Provider>
+  );
+}
+
+function enhancers(){
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  return composeEnhancers(
+    applyMiddleware(thunk)
   );
 }
